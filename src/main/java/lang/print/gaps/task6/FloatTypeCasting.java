302e6b164030d197ec8516feed88fa2bc6c3334f
KenTaR0-gg <kotya.piso@gmail.com>
2025-06-26 02:09:26 +0300

    module 3. task 6

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,7 +2,14 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
-        double r = Math.ceil(numberToBeRounded);
+        double r = Math.round(numberToBeRounded);
         System.out.println(r);
+
+//    }
+
+  //  public static void main(String[] args) {
+    //    FloatTypeCasting floatTypeCasting = new FloatTypeCasting();
+      //  floatTypeCasting.roundNumber(55.1f);
+       // floatTypeCasting.roundNumber(57.9f);
     }
 }
